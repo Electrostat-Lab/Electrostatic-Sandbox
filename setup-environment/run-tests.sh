a40c828f444309a903c5e9ab009ccaea171efd56
pavl_g <scrappers.tm@gmail.com>
2024-05-30 14:15:59 -0400

    setup-environment: testing scripts

---
+++ setup-environment/run-tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+former_working_dir="`pwd`"
+
+##
+# The root directory specified as a command parameter.
+##
+root="${1}"
+
+cd "${root}"
+
+working_dir="`pwd`"
+
+source "${working_dir}/Electrostatic-Sandbox/setup-environment/setup-scripts/variables.sh"
+source "${unit_tests}"
+
+##
+# Run tests on dummy files and then cleanup.
+##
+function run_tests() {
+	test_preparation
+	test_downloader
+	test_extracting
+	cleanup
+}
+
+run_tests
+ping_package
+
+##
+# exit
+##
+cd "${former_working_dir}"
