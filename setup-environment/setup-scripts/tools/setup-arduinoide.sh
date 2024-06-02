0af9883cf24d3ce24875149374fabb9b2c811a42
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2024-06-02 06:31:19 +0300

    Update setup-arduinoide.sh: fixed arduinoide aliases

--- setup-environment/setup-scripts/tools/setup-arduinoide.sh
+++ setup-environment/setup-scripts/tools/setup-arduinoide.sh
@@ -4,17 +4,17 @@ source "./Electrostatic-Sandbox/setup-environment/setup-scripts/variables.sh"
 source "${abstract_scripts}"
 
 function download_arduinoide() {
-	download_package "${download_dir}" "${arduino_url}" ""
+	download_package "${download_dir}" "${arduinoide_url}" ""
 }
 
 function extract_arduinoide() {
-	tar_extract_package "${arduino_provisional_path}" "${sandbox_path}"
+	tar_extract_package "${arduinoide_provisional_path}" "${sandbox_path}"
 }
 
 function cleanup_arduinoide() {
-	rm --verbose "${download_dir}/${arduino_tar_name}"
+	rm --verbose "${download_dir}/${arduinoide_tar_name}"
 }
 
 function create_arduinoide_symbol() {
-	create_sandbox_symbol "${sandbox_path}/${arduino_tar_content}/bin/arduino" "${arduino_symbol}"
+	create_sandbox_symbol "${sandbox_path}/${arduinoide_tar_content}/bin/arduino" "${arduinoide_symbol}"
 }
