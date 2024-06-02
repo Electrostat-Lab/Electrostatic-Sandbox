0df453e4c729174eb7f507135257cc4baa0c278a
pavl_g <scrappers.tm@gmail.com>
2024-06-02 06:22:31 +0200

    setup-scripts: abstract - testing - tools

---
+++ setup-environment/setup-scripts/tools/setup-cmake.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source "./Electrostatic-Sandbox/setup-environment/setup-scripts/variables.sh"
+source "${abstract_scripts}"
+
+function download_cmake() {
+	download_package "${download_dir}" "${cmake_url}" ""
+}
+
+function extract_cmake() {
+	tar_extract_package "${cmake_provisional_path}" "${sandbox_path}"
+}
+
+function cleanup_cmake() {
+	rm --verbose "${download_dir}/${cmake_tar_name}"
+}
+
+function create_cmake_symbol() {
+	create_sandbox_symbol "${sandbox_path}/${cmake_tar_content}/bin/cmake" "${cmake_symbol}"
+}
