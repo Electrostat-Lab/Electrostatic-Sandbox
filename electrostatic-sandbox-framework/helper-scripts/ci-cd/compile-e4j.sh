53337b98c2c9da695e64192655471e81876e3de4
pavl_g <scrappers.tm@gmail.com>
2024-06-05 11:26:07 +0200

    ci-cd/compile-e4j: added the property argument for electrostatic-core home

--- electrostatic-sandbox-framework/helper-scripts/ci-cd/compile-e4j.sh
+++ electrostatic-sandbox-framework/helper-scripts/ci-cd/compile-e4j.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 source "./common-variables.sh"
+
+electrostatic_core_home="${1}"
+
 cd ./electrostatic4j
 
 echo -e "${ORANGE_C} Compiling and Assembling e4j-core"
@@ -8,7 +11,8 @@ echo -e "${ORANGE_C} Compiling and Assembling e4j-core"
           :electrostatic4j-core:generateJavadocJar 
 
 echo -e "${ORANGE_C} Compiling e4j-native"
-./gradlew :electrostatic4j-native:compileX86 \
+./gradlew -PelectrostaticHome="${electrostatic_core_home}" \
+          :electrostatic4j-native:compileX86 \
           :electrostatic4j-native:compileX86_64 && \
           ./gradlew :electrostatic4j-native:copyBinaries && \
           ./gradlew :electrostatic4j-native:build \
