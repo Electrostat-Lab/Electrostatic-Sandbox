b40bb50c4b7fb7f82bf215509335513e95c13468
pavl_g <scrappers.tm@gmail.com>
2024-06-05 11:28:02 +0200

    electrostatic4j/helper-scripts: electrostatic_core_home as vararg

--- electrostatic-sandbox-framework/electrostatic4j/helper-scripts/compile-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source "./helper-scripts/project-impl/variables.sh"
-
-./helper-scripts/project-impl/java-binding/compile-x86.sh "${x86_64}"
-./helper-scripts/project-impl/java-binding/compile-x86.sh "${x86}"
