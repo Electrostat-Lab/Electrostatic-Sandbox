c0ef89c3f80d4f59ff8430672ac10228296d6c26
pavl_g <scrappers.tm@gmail.com>
2024-06-05 22:17:45 +0200

    electrostatic4j: added buildSrc Core

--- electrostatic-sandbox-framework/helper-scripts/ci-cd/test-electrostatic.sh
+++ electrostatic-sandbox-framework/helper-scripts/ci-cd/test-electrostatic.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-./helper-scripts/project-impl/compile-examples.sh "x86-64" "hello_comm.c" 
+./helper-scripts/project-impl/compile-examples.sh "x86-64" "hello_comm.c"
+./helper-scripts/project-impl/compile-examples.sh "x86" "hello_comm.c"
 ./electrostatic-examples/build/x86-64/hello_comm.c.elf
+./electrostatic-examples/build/x86/hello_comm.c.elf
