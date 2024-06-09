3a68fa69898dfb8a14395e7d6ed3bb31db18b3af
pavl_g <scrappers.tm@gmail.com>
2024-06-09 18:43:31 +0200

    compile-all-android.sh: dirty-linking missing essential object files

--- electrostatic-sandbox-framework/helper-scripts/project-impl/compile-all-android.sh
+++ electrostatic-sandbox-framework/helper-scripts/project-impl/compile-all-android.sh
@@ -3,16 +3,16 @@ source "./helper-scripts/project-impl/variables.sh"
 
 ./helper-scripts/project-impl/compile-electrostatic.sh \
         "${CLANG_BIN}" "${CLANGPP_BIN}" \
-        "-target ${ARM_64}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ARM_64}"
+        "-target ${ARM_64} -ldl -lc" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ARM_64}"
 
 ./helper-scripts/project-impl/compile-electrostatic.sh \
         "${CLANG_BIN}" "${CLANGPP_BIN}" \
-        "-target ${ARM_32}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ARM_32}"
+        "-target ${ARM_32} -ldl -lc" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ARM_32}"
 
 ./helper-scripts/project-impl/compile-electrostatic.sh \
         "${CLANG_BIN}" "${CLANGPP_BIN}" \
-        "-target ${ANDROID_x86}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ANDROID_x86}"
+        "-target ${ANDROID_x86} -ldl -lc" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ANDROID_x86}"
 
 ./helper-scripts/project-impl/compile-electrostatic.sh \
         "${CLANG_BIN}" "${CLANGPP_BIN}" \
-        "-target ${ANDROID_x86_64}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ANDROID_x86_64}"
+        "-target ${ANDROID_x86_64} -ldl -lc" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ANDROID_x86_64}"
