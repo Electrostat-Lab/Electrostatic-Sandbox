a3bb4cae471781171e3a4407b5a37339b6cd1ebc
pavl_g <scrappers.tm@gmail.com>
2024-06-09 18:37:24 +0200

    compile-all-android.sh: added missing -target option specifier

--- electrostatic-sandbox-framework/helper-scripts/project-impl/compile-all-android.sh
+++ electrostatic-sandbox-framework/helper-scripts/project-impl/compile-all-android.sh
@@ -3,16 +3,16 @@ source "./helper-scripts/project-impl/variables.sh"
 
 ./helper-scripts/project-impl/compile-electrostatic.sh \
         "${CLANG_BIN}" "${CLANGPP_BIN}" \
-        "${ARM_64}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ARM_64}"
+        "-target ${ARM_64}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ARM_64}"
 
 ./helper-scripts/project-impl/compile-electrostatic.sh \
         "${CLANG_BIN}" "${CLANGPP_BIN}" \
-        "${ARM_32}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ARM_32}"
+        "-target ${ARM_32}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ARM_32}"
 
 ./helper-scripts/project-impl/compile-electrostatic.sh \
         "${CLANG_BIN}" "${CLANGPP_BIN}" \
-        "${ANDROID_x86}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ANDROID_x86}"
+        "-target ${ANDROID_x86}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ANDROID_x86}"
 
 ./helper-scripts/project-impl/compile-electrostatic.sh \
         "${CLANG_BIN}" "${CLANGPP_BIN}" \
-        "${ANDROID_x86_64}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ANDROID_x86_64}"
+        "-target ${ANDROID_x86_64}" "${NDK_TOOLCHAIN_INCLUDES}" "android" "${ANDROID_x86_64}"
