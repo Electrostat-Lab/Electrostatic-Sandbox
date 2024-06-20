c5db1dd650dc975f959f8908c83f1e204f11d9e3
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2024-06-20 15:58:09 -0400

    Update web-controls.js

--- website/web-controls.js
+++ website/web-controls.js
@@ -2,6 +2,12 @@ const expandButton = document.getElementById('expandButton');
 const menu = document.getElementById('navbar-collapse-with-animation');
 
 // toggle events
+expandButton.addEventListener('touchstart', function() {
+  menu.classList.toggle('hidden');
+  menu.classList.toggle('overflow-hidden');
+});
+
 expandButton.addEventListener('click', function() {
   menu.classList.toggle('hidden');
+  menu.classList.toggle('overflow-hidden');
 });
