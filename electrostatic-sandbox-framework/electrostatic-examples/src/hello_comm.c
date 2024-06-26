0b93c1e42e7abeb58b387d27d059b8fa8e88d81f
pavl_g <scrappers.tm@gmail.com>
2024-06-26 05:43:31 -0400

    electrostatic-framework: applied API changes to examples and libraries

--- electrostatic-sandbox-framework/electrostatic-examples/src/hello_comm.c
+++ electrostatic-sandbox-framework/electrostatic-examples/src/hello_comm.c
@@ -5,8 +5,8 @@ comm_protocol serial;
 comm_protocol parallel;
 
 int main() {
-    init_protocol_default(&serial, SERIAL_RS232);
-    init_protocol_default(&parallel, IEEE_1284);
+    init_protocol_default(&serial, SERIAL_RS232, NULL);
+    init_protocol_default(&parallel, IEEE_1284, NULL);
     parallel.open(0);
     serial.open(0);
     return 0;
