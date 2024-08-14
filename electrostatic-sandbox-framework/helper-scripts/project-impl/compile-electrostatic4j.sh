b65864dd72c4b2ffe5942cfe9fc9c2b1ac3b275e
pavl_g <scrappers.tm@gmail.com>
2024-08-14 11:22:48 +0300

    helper-scripts: applied APIs changes

--- electrostatic-sandbox-framework/helper-scripts/project-impl/compile-electrostatic4j.sh
+++ electrostatic-sandbox-framework/helper-scripts/project-impl/compile-electrostatic4j.sh
@@ -6,27 +6,29 @@ source "./helper-scripts/abstract/abstract-compile.sh"
 source "./helper-scripts/abstract/abstract-util.sh"
 source "./helper-scripts/project-impl/variables.sh"
 
-GCC_BIN="${1}"
-GPP_BIN="${2}"
-TOOLCHAIN_HEADERS="${3}"
-java_home="${4}"
-target="${5}"
-system_dir="${6}"
-build_dir="${7}"
+project_dir="${1}"
+commission_lib="${2}"
+GCC_BIN="${3}"
+GPP_BIN="${4}"
+TOOLCHAIN_HEADERS="${5}"
+java_home="${6}"
+target="${7}"
+system_dir="${8}"
+build_dir="${9}"
 
 echo "${build_dir}"
 
 # precompile scripts
-sources=$(find "$(pwd)/${e4j_dir}/src/" -name *.c -o -name *.cpp -o -name *.cxx | tr '\n' ';')
-dependencies=$(find "$(pwd)/${e4j_dir}/dependencies/libs/${system_dir}/${build_dir}" -name *.a | tr '\n' ';')
+sources=$(find "$(pwd)/${project_dir}/src/" -name *.c -o -name *.cpp -o -name *.cxx | tr '\n' ';')
+dependencies=$(find "$(pwd)/${project_dir}/dependencies/libs/${system_dir}/${build_dir}" -name *.a | tr '\n' ';')
 
 getCurrentSystem
 
 JNI_HEADERS_SYSTEM="${JNI_HEADERS}/${system}"
 
-compile "${COMMISSION_LIB_4j}" "${GCC_BIN}" "${GPP_BIN}" "${INPUT_COMPILER_OPTIONS}" \
+compile "${commission_lib}" "${GCC_BIN}" "${GPP_BIN}" "${INPUT_COMPILER_OPTIONS}" \
         "${target}" "${TOOLCHAIN_HEADERS};${JNI_HEADERS};${JNI_HEADERS_SYSTEM}" "${system_dir}/${build_dir}"  \
-        "." "${e4j_dir}" "${sources}" "${dependencies}"
+        "." "${project_dir}" "${sources}" "${dependencies}"
 
-moveFile "${e4j_dir}/cmake-build/${system_dir}/${build_dir}/lib${COMMISSION_LIB_4j}.so" \
-         "${e4j_dir}/build/lib/${system_dir}/${build_dir}/"
+moveFile "${project_dir}/cmake-build/${system_dir}/${build_dir}/lib${commission_lib}.so" \
+         "${project_dir}/build/lib/${system_dir}/${build_dir}/"
