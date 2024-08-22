5fd78218e14c40cc532bd30579bfd7a5ff1e6668
pavl_g <scrappers.tm@gmail.com>
2024-08-22 13:52:46 +0300

    ci-cd/test-electrostatic.sh: parameterized tech demo run

--- electrostatic-sandbox-framework/helper-scripts/ci-cd/test-electrostatic.sh
+++ electrostatic-sandbox-framework/helper-scripts/ci-cd/test-electrostatic.sh
@@ -2,8 +2,10 @@
 
 source "./helper-scripts/project-impl/variables.sh"
 
-./helper-scripts/project-impl/compile-examples.sh "${TARGET_x86_64}" "hello_comm.c" "linux" "${x86_64}"
-./helper-scripts/project-impl/compile-examples.sh "${TARGET_x86}" "hello_comm.c" "linux" "${x86}"
+example="${1}"
 
-./electrostatic-examples/build/linux/x86-64/hello_comm.c.elf
-./electrostatic-examples/build/linux/x86/hello_comm.c.elf
+./helper-scripts/project-impl/compile-examples.sh "${TARGET_x86_64}" "${example}" "linux" "${x86_64}"
+./helper-scripts/project-impl/compile-examples.sh "${TARGET_x86}" "${example}" "linux" "${x86}"
+
+./electrostatic-examples/build/linux/x86-64/${example}.elf
+./electrostatic-examples/build/linux/x86/${example}.elf
