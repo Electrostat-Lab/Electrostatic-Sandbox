009754bf1c423965e36e75b21821aafd395d074f
pavl_g <scrappers.tm@gmail.com>
2024-09-03 18:52:35 +0100

    electrostatic-sandbox scripting: commented x86 runtime

--- electrostatic-sandbox-framework/helper-scripts/ci-cd/test-electrostatic.sh
+++ electrostatic-sandbox-framework/helper-scripts/ci-cd/test-electrostatic.sh
@@ -5,7 +5,7 @@ source "./helper-scripts/project-impl/variables.sh"
 example="${1}"
 
 ./helper-scripts/project-impl/compile-examples.sh "${TARGET_x86_64}" "${example}" "linux" "${x86_64}"
-./helper-scripts/project-impl/compile-examples.sh "${TARGET_x86}" "${example}" "linux" "${x86}"
+#./helper-scripts/project-impl/compile-examples.sh "${TARGET_x86}" "${example}" "linux" "${x86}"
 
 ./electrostatic-examples/build/linux/x86-64/${example}.elf
-./electrostatic-examples/build/linux/x86/${example}.elf
+#./electrostatic-examples/build/linux/x86/${example}.elf
