56b3fe808c31e1f4484731544471a71034f29e62
pavl_g <scrappers.tm@gmail.com>
2024-09-13 21:29:17 +0300

    algebra/switching: added switching MUX library

--- electrostatic-sandbox-framework/electrostatic-core/src/include/electrostatic/algorithm/arithmos/algebra/switching.h
+++ electrostatic-sandbox-framework/electrostatic-core/src/include/electrostatic/algorithm/arithmos/algebra/switching.h
@@ -17,11 +17,23 @@ extern "C" {
 #define SWITCHING_TYPE uint8_t
 #endif
 
+typedef enum filter_type {
+    NO_FILTER,
+    NOT_GATE,
+} filter_type;
+
+typedef struct switching_filter {
+    uint8_t (*filter)(SWITCHING_TYPE input, SWITCHING_TYPE *output);
+    filter_type type;
+} switching_filter;
+
+uint8_t init_switching_filter(switching_filter *filter);
 uint8_t switching_not(SWITCHING_TYPE input, SWITCHING_TYPE *output);
 uint8_t switching_and(SWITCHING_TYPE **inputs, SWITCHING_TYPE *output);
 uint8_t switching_or(SWITCHING_TYPE **inputs, SWITCHING_TYPE *output);
 uint8_t switching_xor(SWITCHING_TYPE **inputs, SWITCHING_TYPE *output);
-uint8_t switching_multiplex(SWITCHING_TYPE **inputs, SWITCHING_TYPE *output, uint8_t line_select);
+uint8_t switching_multiplex(SWITCHING_TYPE **inputs, SWITCHING_TYPE *output, SWITCHING_TYPE **selects, struct switching_filter ***filters);
+uint8_t switching_multiplex_bitlevel(SWITCHING_TYPE input, SWITCHING_TYPE *output, SWITCHING_TYPE line_select);
 uint8_t switching_demultiplex(SWITCHING_TYPE input, SWITCHING_TYPE **outputs, uint8_t line_select);
 
 #ifdef __cplusplus
