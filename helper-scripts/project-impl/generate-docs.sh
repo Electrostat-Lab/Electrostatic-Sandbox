b8bbb808bad2cda40821ecd861965aedb958305a
Pavly Gerges (pavl_g) <60224159+pavly-gerges@users.noreply.github.com>
2024-10-24 20:40:25 +0300

    Update generate-docs.sh

--- helper-scripts/project-impl/generate-docs.sh
+++ helper-scripts/project-impl/generate-docs.sh
@@ -2,6 +2,8 @@
 
 source "./helper-scripts/abstract/abstract-doxygen-generator.sh"
 
+sudo apt install doxygen
+
 generate_default_config "$(pwd)/electrostatic-sandbox-framework/docs/doxygen/" "DOC-CONFIG"
 
 generate_docs "$(pwd)/electrostatic-sandbox-framework/docs/doxygen/" "DOC-CONFIG"
