43d0d3c7b24815a9cd097bfd8dafd2a2dfc50bf2
pavl_g <scrappers.tm@gmail.com>
2024-10-28 03:35:19 +0300

    Applied compilation API changes on the CI/CD and studio configs

--- helper-scripts/ci-cd/test-electrostatic-mcu.sh
+++ helper-scripts/ci-cd/test-electrostatic-mcu.sh
@@ -3,15 +3,15 @@
 source "./helper-scripts/project-impl/variables.sh"
 
 example="${1}"
-executable_suffix="${2}"
+executable="${2}"
 target="${3}"
 chip_alias=${4}
 build_dir="${5}"
 
 ./helper-scripts/project-impl/compile-examples-mcu.sh "-mmcu=${target}" \
-    "${example}" "${executable_suffix}" "${build_dir}" "${target}" "OFF"
+    "${example}" "${executable}" "${build_dir}" "${target}" "OFF"
 
 root_build_path="$(pwd)/electrostatic-sandbox-framework/electrostatic-examples"
 
-./helper-scripts/project-impl/upload-hex.sh "techdemo-${executable_suffix}-a.elf" \
-    "${root_build_path}/build/${build_dir}/${target}" "${chip_alias}" ""
+./helper-scripts/project-impl/upload-hex.sh "${executable}.elf" \
+    "${root_build_path}/cmake-build/${build_dir}/${target}" "${chip_alias}" ""
