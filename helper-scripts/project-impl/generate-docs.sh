009b6caedc58875420d83d1fa220900b39dca1c9
Pavly Gerges (pavl_g) <60224159+pavly-gerges@users.noreply.github.com>
2024-12-20 09:17:04 +0200

    Update generate-docs.sh: commands to generate dot architectural paradigm

--- helper-scripts/project-impl/generate-docs.sh
+++ helper-scripts/project-impl/generate-docs.sh
@@ -5,3 +5,11 @@ source "./helper-scripts/abstract/abstract-doxygen-generator.sh"
 generate_default_config "$(pwd)/electrostatic-sandbox-framework/docs/doxygen/" "DOC-CONFIG"
 
 generate_docs "$(pwd)/electrostatic-sandbox-framework/docs/doxygen/" "DOC-CONFIG"
+
+temp=$(pwd)
+
+cd "./electrostatic-sandbox-framework/docs/"
+
+dot -Tsvg -o electrostatic-sdk.svg "./electrostatic-sdk.dot"
+
+cd "${temp}"
