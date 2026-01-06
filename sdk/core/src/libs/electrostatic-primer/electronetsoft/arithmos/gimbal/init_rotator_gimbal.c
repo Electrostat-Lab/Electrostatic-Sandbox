f0916fd7601b62ecb20a231bf6f004e33ff9741c
pavl-g <scrappers.tm@gmail.com>
2026-01-06 11:11:33 +0200

    arithmos/gimbal: indentations for init_rotator_gimbal

--- sdk/core/src/libs/electrostatic-primer/electronetsoft/arithmos/gimbal/init_rotator_gimbal.c
+++ sdk/core/src/libs/electrostatic-primer/electronetsoft/arithmos/gimbal/init_rotator_gimbal.c
@@ -10,59 +10,59 @@ status_code init_rotator_gimbal(vector3d axis, matrix *__rotator,
     }
 
     if (get_vec_gimbal(axis) == GIMBAL_Z) {
-    // rotate around z-axis
-    // pre-processing automata
-    // Let the X-axis in the R(3) space be the X-axis in the 2D projection.
-    // Let the Y-axis in the R(3) space be the Y-axis in the 2D projection.
+        // rotate around z-axis
+        // pre-processing automata
+        // Let the X-axis in the R(3) space be the X-axis in the 2D projection.
+        // Let the Y-axis in the R(3) space be the Y-axis in the 2D projection.
 
-    // work in XY plane
-    __rotator->element[0][0] = vector2d_cos(angle);
-    __rotator->element[0][1] = -vector2d_sin(angle);
-    __rotator->element[1][0] = vector2d_sin(angle);
-    __rotator->element[1][1] = vector2d_cos(angle);
-    __rotator->element[2][2] = 1;
+        // work in XY plane
+        __rotator->element[0][0] = vector2d_cos(angle);
+        __rotator->element[0][1] = -vector2d_sin(angle);
+        __rotator->element[1][0] = vector2d_sin(angle);
+        __rotator->element[1][1] = vector2d_cos(angle);
+        __rotator->element[2][2] = 1;
 
-    if (NULL != gimbal && NULL != angle1) {
-        gimbal->z_gimbal += angle;
-        *angle1 = gimbal->z_gimbal;
-    }
+        if (NULL != gimbal && NULL != angle1) {
+            gimbal->z_gimbal += angle;
+            *angle1 = gimbal->z_gimbal;
+        }
 
     } else if (get_vec_gimbal(axis) == GIMBAL_Y) {
-    // rotate around y-axis
-    // pre-processing automata
-    // Let the X-axis in the R(3) space be the X-axis in the 2D projection.
-    // Let the Z-axis in the R(3) space be the Y-axis in the 2D projection.
+        // rotate around y-axis
+        // pre-processing automata
+        // Let the X-axis in the R(3) space be the X-axis in the 2D projection.
+        // Let the Z-axis in the R(3) space be the Y-axis in the 2D projection.
 
-    // work in XZ plane
-    __rotator->element[0][0] = vector2d_cos(angle);
-    __rotator->element[0][2] = -vector2d_sin(angle);
-    __rotator->element[2][0] = vector2d_sin(angle);
-    __rotator->element[2][2] = vector2d_cos(angle);
-    __rotator->element[1][1] = 1;
+        // work in XZ plane
+        __rotator->element[0][0] = vector2d_cos(angle);
+        __rotator->element[0][2] = -vector2d_sin(angle);
+        __rotator->element[2][0] = vector2d_sin(angle);
+        __rotator->element[2][2] = vector2d_cos(angle);
+        __rotator->element[1][1] = 1;
 
-    if (NULL != gimbal && NULL != angle1) {
-        gimbal->y_gimbal += angle;
-        *angle1 = gimbal->y_gimbal;
-    }
+        if (NULL != gimbal && NULL != angle1) {
+            gimbal->y_gimbal += angle;
+            *angle1 = gimbal->y_gimbal;
+        }
 
     } else if (get_vec_gimbal(axis) == GIMBAL_X) {
 
-    // rotate around x-axis
-    // pre-processing automata
-    // Let the Z-axis in the R(3) space be the X-axis in the 2D projection.
-    // Let the Y-axis in the R(3) space be the Y-axis in the 2D projection.
+        // rotate around x-axis
+        // pre-processing automata
+        // Let the Z-axis in the R(3) space be the X-axis in the 2D projection.
+        // Let the Y-axis in the R(3) space be the Y-axis in the 2D projection.
 
-    // work in ZY plane
-    __rotator->element[2][2] = vector2d_cos(angle);
-    __rotator->element[2][1] = -vector2d_sin(angle);
-    __rotator->element[1][2] = vector2d_sin(angle);
-    __rotator->element[1][1] = vector2d_cos(angle);
-    __rotator->element[0][0] = 1;
+        // work in ZY plane
+        __rotator->element[2][2] = vector2d_cos(angle);
+        __rotator->element[2][1] = -vector2d_sin(angle);
+        __rotator->element[1][2] = vector2d_sin(angle);
+        __rotator->element[1][1] = vector2d_cos(angle);
+        __rotator->element[0][0] = 1;
 
-    if (NULL != gimbal && NULL != angle1) {
-        gimbal->x_gimbal += angle;
-        *angle1 = gimbal->x_gimbal;
-    }
+        if (NULL != gimbal && NULL != angle1) {
+            gimbal->x_gimbal += angle;
+            *angle1 = gimbal->x_gimbal;
+        }
 
     } else {
         return EINCOMPATTYPE;
