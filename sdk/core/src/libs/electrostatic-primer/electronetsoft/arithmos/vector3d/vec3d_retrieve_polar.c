670b0b6ca04349dd560d4697ab06341bd47c5a4a
pavl-g <scrappers.tm@gmail.com>
2026-07-21 00:29:24 +0300

    vector3d/vec3d_retrieve_polar: simplification of the algorithm

--- sdk/core/src/libs/electrostatic-primer/electronetsoft/arithmos/vector3d/vec3d_retrieve_polar.c
+++ sdk/core/src/libs/electrostatic-primer/electronetsoft/arithmos/vector3d/vec3d_retrieve_polar.c
@@ -4,45 +4,27 @@
 status_code vec3d_retrieve_polar(vector3d v, vec3d_polar *out,
                                  vec3d_processors *processors) {
     if (rvalue(out) == NULL) {
+        vec3d_safe_exec_failure(&vec3d_retrieve_polar,
+                                processors, EUNDEFINEDBUFFER);
         return EUNDEFINEDBUFFER;
     }
 
-    vec2d_polar polar_vector;
     status_code __code;
 
-    vector2d v_xy = {
-        .x = v.x,
-        .y = v.y,
-    };
-    vector2d v_z = {
-        .y = v.z,
-    };
-
+    // the radius component
     __code = vec3d_length(v, &(out->r), processors);
     if (PASS != __code) {
+        vec3d_safe_exec_failure(&vec3d_retrieve_polar,
+                                processors, __code);
         return __code;
     }
 
-    __code = vec2d_retrieve_polar(v_xy, &polar_vector, NULL);
-    if (PASS != __code) {
-        return __code;
-    }
+    vec_component v_xy = vector2d_sqrt((v.x * v.x) + (v.y * v.y));
 
-    out->phi = polar_vector.phi;
+    out->phi = vector2d_atan2(v.y, v.x);
+    out->theta = vector2d_atan2(v_xy, v.z);
 
-    // find the resultant vector component of the XY plane
-    // put the result into the v_z (x component)
-    __code = vec2d_length(v_xy, &(v_z.x), NULL);
-    if (PASS != __code) {
-        return __code;
-    }
-
-    __code = vec2d_retrieve_polar(v_z, &polar_vector, NULL);
-    if (PASS != __code) {
-        return __code;
-    }
-
-    out->theta = polar_vector.phi;
+    vec3d_safe_exec_success(v, &vec3d_retrieve_polar, processors);
 
     return PASS;
 }
